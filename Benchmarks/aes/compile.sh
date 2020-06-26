5e32af05240e89baa875618e9454d10f648d4bd4
sygc-pets <sygc.pets@gmail.com>
2020-06-25 19:10:35 -0700

    remove files not related to TinyGarble2

--- Benchmarks/aes/compile.sh
+++ Benchmarks/aes/compile.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
-mkdir -p syn
-design_vision -no_gui -f aes.dcsh
+echo "Use -d to compile with Synopsys Design Compiler"
 
-rm *.pvl *.syn *.mr *.log *.svf
+mkdir -p syn
+
+if [ $1 ] && [ $1 = "-d" ]; then
+	design_vision -no_gui -f aes.dcsh
+	rm *.pvl *.syn *.mr *.log *.svf
+else
+	yosys -c aes_1cc.tcl
+fi
+
+for verilogfile in syn/*.v
+do
+  empfile=${verilogfile%.*}.emp
+  ../../Verilog2EMP/bin/V2EMP_Main -i $verilogfile -o $empfile --log2std 
+done 
--- Benchmarks/compile_all.sh
+++ Benchmarks/compile_all.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 mkdir -p syn_all
-for d in *
-do
-	( cd "$d" && ./compile.sh && rm *.pvl *.syn *.mr *.log *.svf && cd ".." && cp ${d%/*}/syn/*.v syn_all)	
-done
-rm *.log
+echo "./compile_all.sh. use -s to skip synthesis" 
+if [ "$1" == "-s" ]
+then 
+	echo "skipping synthesis"
+	for d in *
+	do
+		( cp ${d%/*}/syn/*.v syn_all & )
+	done
+else 
+	for d in *
+	do
+		( cd "$d" && ./compile.sh -d && rm *.pvl *.syn *.mr *.log *.svf && cd ".." && cp ${d%/*}/syn/*.v syn_all)
+	done
+fi
+#for verilogfile in syn_all/*.v
+#do
+#  empfile=${verilogfile%.*}.emp
+#  ../Verilog2EMP/bin/V2EMP_Main -i $verilogfile -o $empfile --log2std &
+#done
+#wait 
